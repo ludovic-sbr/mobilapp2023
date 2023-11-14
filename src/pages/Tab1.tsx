c2491f47575df25aaf78f6bd620d375d2d0c6b0b
Ludovic Sbr <sobrero.ludovic@gmail.com>
2023-11-14 10:55:40 +0100

    chore : adding button and fetch catfact api

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,19 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {IonButton, IonContent, IonPage} from '@ionic/react';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+
+    const fetchData = async () => {
+        const result = await fetch("https://catfact.ninja/fact");
+
+        console.log(result.json());
+    }
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
+        <IonContent>
+            <IonButton onClick={() => fetchData()}>Default</IonButton>
+        </IonContent>
     </IonPage>
   );
 };
